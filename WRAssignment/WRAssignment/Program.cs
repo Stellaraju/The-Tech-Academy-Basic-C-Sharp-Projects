2abefd07ccf7fd87f878a832044ac3a9b9b0fd66
Stellaraju <stellaraju2004@gmail.com>
2025-01-28 15:12:28 -0800

    Writing an Running Code C# Assignment

---
+++ WRAssignment/WRAssignment/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Print the welcome message
+        Console.WriteLine("Welcome to Acme Accounting Systems");
+
+        // Print the additional message with escaped quotes for "accounting"
+        Console.WriteLine("Remember, we’re \"accounting\" on you!");
+
+        // Optional: Wait for the user to press Enter before closing the console window
+        Console.ReadLine();
+    }
+}
