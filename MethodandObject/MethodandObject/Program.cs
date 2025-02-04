3ddc6805a89473aa30f2a224acac7540a8e72c8d
Stellaraju <stellaraju2004@gmail.com>
2025-02-04 13:55:40 -0800

    MethodObjectAssignment

---
+++ MethodandObject/MethodandObject/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+public class Person
+{
+    // Properties for First Name and Last Name
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    // Method to print full name
+    public void SayName()
+    {
+        Console.WriteLine($"Name: {FirstName} {LastName}");
+    }
+}
+
+public class Employee : Person
+{
+    // Additional property for Employee ID
+    public int Id { get; set; }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Instantiate an Employee object with sample names
+        Employee employee = new Employee
+        {
+            FirstName = "Stellamary",
+            LastName = "Assirvatham"
+        };
+
+        // Call SayName() method from the Person class
+        employee.SayName();
+        Console.ReadLine();
+    }
+}
